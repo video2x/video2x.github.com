f2647fe50eeb817fdf5d98c033d15fc992ef6531
video2x <39461773+video2x@users.noreply.github.com>
2018-05-20 21:46:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.submit').click(function() {
+		link = $('.link').val();
+		video = $('video');
+		source = $('.source');
+		source.attr('src',link);
+		video[0].src=link;
+		video[0].load();
+		video[0].play();
+	});
+	videojs('my-video').ready(function() {
+		this.hotkeys({
+			volumeStep: 0.1,
+			seekStep: 15,
+			enableMute: true,
+			enableFullscreen: true,
+			enableNumbers: false,
+			enableVolumeScroll: true,
+		});
+	});
+});
